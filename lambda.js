d68d074a2fb0bb877f6152e5adeb157a1c7f97a9
Siddharth Suresh <siddh.suresh@gmail.com>
2024-11-12 18:59:50 +0530

    Update lambda.js

--- lambda.js
+++ lambda.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-    console.log(process.env)
+    console.log("Hello World 2")
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
